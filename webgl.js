7dff41a446dee0a64dd31d5869f03cc268bc3ef0
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-09-24 18:39:06 +0200

    dat-gui

--- webgl.js
+++ webgl.js
@@ -3,11 +3,11 @@ $(function() {
 		Detector.addGetWebGLMessage();
 	}
 	var width = $(window).width(), height = $(window).height();
-	var container = $("<div>").appendTo("body");
+	var container = $("body");
 	var renderer = new THREE.WebGLRenderer({antialias: true});
 	var scene = new THREE.Scene();
 	
-	var ground = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000, 200, 200), new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true}));
+	var ground = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000, 200, 200), new THREE.MeshBasicMaterial({color: 0, wireframe: true}));
 	ground.rotation.x = Math.PI/2;	//Normals up?
 	
 	scene.add(ground);
@@ -16,19 +16,26 @@ $(function() {
 		fov: 60,
 		aspect: width/height,
 		lookSpeed: 0.1,
-		movementSpeed: 1000,
-		noFly: false
+		movementSpeed: 1000
 	});
 	camera.position.y = 200;
 	renderer.setSize(width, height);
 	
 	container.append(renderer.domElement);
 	
+	var gui = new DAT.GUI();
+	gui.add(ground.materials[0].color, "r", 0, 1);
+	gui.add(ground.materials[0].color, "g", 0, 1);
+	gui.add(ground.materials[0].color, "b", 0, 1);
+	
 	var stats = new Stats();
 	stats.domElement.style.position = "absolute";
 	stats.domElement.style.bottom = 0;
 	stats.domElement.style.right = 0;
 	container.append(stats.domElement);
+	
+	$("div").hover(function() {camera.activeLook = false});
+	$("> canvas", container).hover(function() {camera.activeLook = true});
 	
 	frame();
 	
