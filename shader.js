95aad413c7e0572ab3a49d8258f87424bee71c4b
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-09-27 15:00:04 +0200

    Revert "Multiple materials (broken)"
    This reverts commit f26af4409ffc820946a67cb3859122be60b32ff6.

--- shader.js
+++ shader.js
@@ -1,8 +1,13 @@
 vertexShader = [
 "uniform float phase;",
+"varying vec3 vLightWeighting;",
+THREE.ShaderChunk["lights_pars_vertex"],
 
 "void main() {",
 	"float z = 50.0 * sin(position.y / 50.0 + phase);",
-	"gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, z, 1);",
+	"vec4 mvPosition = modelViewMatrix * vec4(position.x, position.y, z, 1);",
+	"vec3 transformedNormal = normalize(normalMatrix * normal);",
+	THREE.ShaderChunk["lights_vertex"],
+	"gl_Position = projectionMatrix * mvPosition;",
 "}"
 ].join("\n");
