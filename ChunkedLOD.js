a61fd37e20778e614e170434ca4dd85f55317dbe
Bc. Jan Kaláb <pitlicek@gmail.com>
2012-04-09 15:59:22 +0200

    Texture loading, broken

--- ChunkedLOD.js
+++ ChunkedLOD.js
@@ -8,11 +8,23 @@ THREE.ChunkedLOD = function(x1, y1, x2, y2, shiftx, shifty, grid, level) {
 	this.position.z += shifty;
 	//this.position.y = 300 * level + Math.random() * 150;	//For debuging,
 	//console.log(w, h);
-	this.terrain = new THREE.Mesh(grid, new THREE.MeshBasicMaterial({color: Math.random() * 0xffffff, fog: false, wireframe: false}));
-	var scale = 1 / Math.pow(2, level);
-	this.terrain.scale = new THREE.Vector3(scale, scale, scale);
-	this.terrain.visible = false;
-	this.add(this.terrain);
+	var shader = THREE.ShaderUtils.lib["normal"];
+	shader.uniforms["tDisplacement"].texture = THREE.ImageUtils.loadTexture("tile?w=" + w + "&h=" + h + "&x=" + x1 +"&y=" + y1 +"&r=256", new THREE.UVMapping(), function() {
+		//shader.uniforms["tDisplacement"].texture.image.type = THREE.UnsignedShortType;
+		//shader.uniforms["tDisplacement"].texture.needsUpdate = true;
+		shader.uniforms["tDisplacement"].texture.image.width = 256;
+		shader.uniforms["tDisplacement"].texture.image.height = 256;
+		//shader.uniforms["tNormal"].texture = new THREE.Texture(THREE.ImageUtils.getNormalMap(shader.uniforms["tDisplacement"].texture.image), 1024);
+		//shader.uniforms["tNormal"].texture.needsUpdate = true;
+		shader.uniforms["uDisplacementBias"].value = -2048;
+		shader.uniforms["uDisplacementScale"].value = 12000;
+		this.terrain = new THREE.Mesh(grid, new THREE.ShaderMaterial({fragmentShader: shader.fragmentShader, vertexShader: shader.vertexShader, uniforms: shader.uniforms, lights: false, wireframe: true, fog: false}));
+		var scale = 1 / Math.pow(2, level);
+		this.terrain.scale = new THREE.Vector3(scale, scale, scale);
+		this.terrain.visible = false;
+		this.add(this.terrain);
+		console.log(".")
+	});
 	this.LODs = new THREE.Object3D();
 	if (w / 2 > 256 && h / 2 > 256) {
 		var halfx = x1 + w / 2, halfy = y1 + h / 2;
@@ -32,7 +44,13 @@ THREE.ChunkedLOD.prototype.update = function(camera) {
 	var rho = (this.delta * THREE.K) / this.matrixWorld.getPosition().distanceTo(camera.position);
 	//console.log(this.delta, rho, THREE.tau);
 	//console.log(this.delta, this.worldpos.distanceTo(camera.position));
-	if (rho <= THREE.tau || this.LODs.children.length != 4) {
+	var rdy = 0;
+	for (var i = 0; i < this.LODs.children.length; i++) {
+		if (this.LODs.children[i].terrain) {
+			rdy++;
+		}
+	}
+	if (rho <= THREE.tau || rdy != 4) {
 		this.terrain.visible = true;
 		THREE.SceneUtils.showHierarchy(this.LODs, false);
 	} else {
